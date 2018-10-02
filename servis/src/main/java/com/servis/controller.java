2389bc23d9c511f606382a92e12b444b1d1797ec
Patrik Krajčír <43519912+patrikkr@users.noreply.github.com>
2018-10-02 13:47:18 +0200

    Add files via upload
    2.10update

---
+++ servis/src/main/java/com/servis/controller.java
@@ -0,0 +1,12 @@
+package com.servis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	@GetMapping("/")
+	  public String hello() {
+	    return "Hello world";
+	  }
+}
